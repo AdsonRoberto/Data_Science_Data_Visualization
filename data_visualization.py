65c20a68193e2be022cafdc80b2db49c96cfe8d9
AdsonRoberto <adson.uece@gmail.com>
2022-07-21 17:04:15 -0300

    adicionando titulo e legenda no grafico

--- data_visualization.py
+++ data_visualization.py
@@ -4,3 +4,17 @@ x = [1 , 2]
 y = [2 , 3]
 plt.plot(x, y)
 plt.show
+
+#adicionado pontos
+x = [1 , 2 , 5]
+y = [2 , 3, 7]
+
+#adicionado titulo
+plt. title ("grafico usando python")
+
+#adicionando legenda nos eixos
+plt.xlabel("eixo X")
+plt.ylabel("eixo y")
+
+plt.plot(x, y)
+plt.show
